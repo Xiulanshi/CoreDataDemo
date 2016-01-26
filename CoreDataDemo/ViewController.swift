78b03125fc233f400d62c356e40dfbe9b2cb9786
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-26 17:06:27 -0500

    Updated

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Xiulan Shi on 1/26/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
